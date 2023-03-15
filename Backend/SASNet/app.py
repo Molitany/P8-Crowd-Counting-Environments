4958f6c709dd8569258d6d747a97c6cc0c098df2
Molitany <thor@beregaard.dk>
2023-03-15 12:27:02 +0100

    Brightness histogram and model chosen based on it

--- Backend/SASNet/app.py
+++ Backend/SASNet/app.py
@@ -13,10 +13,12 @@ from torch.utils.data import Dataset
 from model import SASNet
 from PIL import Image
 
+
 warnings.filterwarnings('ignore')
 
 # define the GPU id to be used
-#os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
 
 class data(Dataset):
     def __init__(self, img, transform=None):
@@ -32,9 +34,10 @@ class data(Dataset):
         image = image.convert('RGB')
         if self.transform is not None:
             image = self.transform(image)
-      
+
         image = torch.Tensor(image)
         return image
+
 
 def loading_data(img):
     # the augumentations
@@ -44,7 +47,8 @@ def loading_data(img):
     ])
     # dcreate  the dataset
     test_set = data(img=img, transform=transform)
-    test_loader = DataLoader(test_set, batch_size=1, num_workers=0, shuffle=False, drop_last=False)
+    test_loader = DataLoader(test_set, batch_size=1,
+                             num_workers=0, shuffle=False, drop_last=False)
 
     return test_loader
 
@@ -53,33 +57,35 @@ def predict(img, model_path):
     if img is None:
         return "No image selected", plt.figure()
     """the main process of inference"""
+    img.show()
     test_loader = loading_data(img)
-    #model = SASNet()
     model = SASNet().cpu()
     # load the trained model
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(
+        model_path, map_location=torch.device('cpu')))
     print('successfully load model from', model_path)
 
     with torch.no_grad():
         model.eval()
 
         for vi, data in enumerate(test_loader, 0):
-          img = data
-          #img = img.cuda()
-          img = img.cpu()
-          pred_map = model(img)
-          pred_map = pred_map.data.cpu().numpy()
-          for i_img in range(pred_map.shape[0]):
-            pred_cnt = np.sum(pred_map[i_img]) / 1000
+            img = data
+            # img = img.cuda()
+            img = img.cpu()
+            pred_map = model(img)
+            pred_map = pred_map.data.cpu().numpy()
+            for i_img in range(pred_map.shape[0]):
+                pred_cnt = np.sum(pred_map[i_img]) / 1000
 
-            den_map = np.squeeze(pred_map[i_img])
-            fig = plt.figure(frameon=False)
-            ax = plt.Axes(fig, [0., 0., 1., 1.])
-            ax.set_axis_off()
-            fig.add_axes(ax)
-            ax.imshow(den_map, aspect='auto')
-            
-            return int(np.round(pred_cnt, 0)), fig
+                den_map = np.squeeze(pred_map[i_img])
+                fig = plt.figure(frameon=False)
+                ax = plt.Axes(fig, [0., 0., 1., 1.])
+                ax.set_axis_off()
+                fig.add_axes(ax)
+                ax.imshow(den_map, aspect='auto')
+
+                return int(np.round(pred_cnt, 0)), fig
+
 
 def subtract_images(img1_path, img2_path):
     """
@@ -90,8 +96,8 @@ def subtract_images(img1_path, img2_path):
     img1 = cv2.imread(img1_path)
     img2 = cv2.imread(img2_path)
     # take height and width
-    height1, width1 = img1.shape[0],img1.shape[1]
-    height2, width2 = img2.shape[0],img2.shape[1]    
+    height1, width1 = img1.shape[0], img1.shape[1]
+    height2, width2 = img2.shape[0], img2.shape[1]
     # find minimum of height and widthe
     min_height = min(height1, height2)
     min_width = min(width1, width2)
@@ -103,11 +109,24 @@ def subtract_images(img1_path, img2_path):
     # convert image to PIL format and convert to PIL image for SASNet
     img = cv2.cvtColor(img_sub, cv2.COLOR_BGR2RGB)
     im_pil = Image.fromarray(img)
+    im_pil.show()
     return im_pil
 
-sub_img = subtract_images("Crosswalk/frame0.jpg", "Crosswalk/frame100.jpg")
-sub_img.show()
-labelA, fig = predict(sub_img, "SHHA.pth")
-labelB, fig = predict(sub_img, "SHHB.pth")
-print(f"SHHA: {labelA}, SHHB: {labelB}")
+
+def get_brightness(img):
+    im = img.convert('L')
+    hst = im.histogram()
+    hst = hst[50:]
+    return hst
+
+def choose_model(sub_img):
+    hst = get_brightness(sub_img)
+    if sum(hst) > 60000:
+        return "SHHA.pth"
+    else:
+        return "SHHB.pth"
+
+sub_img = subtract_images("Crosswalk/frame233.jpg", "Crosswalk/frame0.jpg")
+labelA, fig = predict(Image.open("Crosswalk/frame0.jpg"), choose_model(sub_img))
+print(f"{choose_model(sub_img)}: {labelA}")
 plt.show()
