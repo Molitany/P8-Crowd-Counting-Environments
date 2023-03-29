d8d43ca9c8ca6046f116d73cdcd0eea151b22bb4
Drejer1 <63230715+Drejer1@users.noreply.github.com>
2023-03-29 10:29:05 +0200

    Frontend receives and display image
    The frontend can now display an image it receives from the backend.
    Still need to clean the code before a potential merge.

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.responses import FileResponse
 from app.core.config import settings
 
 import pandas as pd
 import time
-
-
+import base64
 def get_application():
     _app = FastAPI(title=settings.PROJECT_NAME)
 
@@ -27,12 +26,32 @@ app = get_application()
 async def root():
     return {"message": "Hello World"}
 
+
+
+
 @app.websocket("/density")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
+        data = await websocket.receive_text()
+        print("received text")
         try: 
-            status = pd.read_csv('app/dummydata.csv').to_dict()
+            with open("app/testPictureK.png", "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+                print(encoded_string)
+        except Exception as e:
+            print('error', e)
+            break
+        await websocket.send_json({"image" :encoded_string})
+
+
+status = pd.read_csv('app/dummydata.csv').to_dict()
+
+@app.websocket("/densty")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        try: 
             await websocket.send_json(status)
             time.sleep(10)
         except Exception as e:
--- Frontend/components/DisplayImage.tsx
+++ Frontend/components/DisplayImage.tsx
@@ -1,37 +1,98 @@
 import React,{useEffect,useState} from 'react'
-import {View, Image, StyleSheet,Text} from 'react-native';
+import {View, Image, StyleSheet,Text,Button, } from 'react-native';
+
+
+
 
 const DisplayImage = () => {
     const [serverMessages,setServerMessages] = useState([String]);
     const [connectedToServer,setConnectedToserver] = useState(false) ;
     const [serverState, setServerState] = useState('Loading...');
+    const [imageReceived,setImageReceived] = useState(false)
+    const [image, setImage] = useState(String);
     const ws = React.useRef(new WebSocket('ws://localhost:8000/density')).current;
     useEffect(() =>{
             let serverMessagesList = [String];
             ws.onopen = () => {
                 setConnectedToserver(true);
+                console.log("Connected")
             }
             ws.onclose = (e) => {
                 setConnectedToserver(false);
+                console.log("Lost connection")
             }
             ws.onerror = (e) => {
                 setServerState(e.type);
             }
             ws.onmessage = (e) => {
+                const js = JSON.parse(e.data);
+                receiveMessage(js);
                 serverMessagesList.push(e.data);
-                setServerMessages([...serverMessagesList])
+                setServerMessages([...serverMessagesList]);
             };
         },[]
     
     )
+    const asyncSetImage = (js:{image:string}): Promise<string> =>{
+        return new Promise((resolve, reject)=>{
+            const PNG = js.image.replace(/\s/g, '');
+            resolve(PNG)
+        }) 
+    }
+
+
+
+    const receiveMessage = (js : {image:string}) => {
+        asyncSetImage(js).then((data:string ) =>{
+            setImage(data)
+
+        });
+        console.log(image);
+        setImageReceived(true);
+    }
+
+    const sendToSocket = () => {
+        ws.send("Hello World");
+        
+    }
+    
+    function ImageToDisplay() {
+        if (imageReceived) {
+            return (
+                <div>
+                    <View>
+                        <Image source={{ uri: `data:image/jpeg;base64,${image}` }} style={{ width: 300, height: 400 }} />
+                    </View>
+
+                </div>
+            )
+        } else {
+
+            return (
+                <div>
+                    <Button
+                        onPress={sendToSocket}
+                        title="Send to server"
+                        color="#241584"
+                        accessibilityLabel="Learn more about this purple button"
+                    />
+                    <Image
+                        style={styles.picture}
+                        source={require('../assets/images/testPictureDDD.png')} />
+                </div>
+            )
+        }
+    }
+
 
     return (
         <View style={styles.container}>
-            {connectedToServer ? (<Image 
-                style= {styles.picture}
-                source={require('../assets/images/testPictureDDD.png')}/>
+            {connectedToServer ? (
+                <ImageToDisplay/>
                 ) : (
-                <Text style = {{color:'#fff'}}>not connected</Text>)
+                <Text style = {{color:'#fff'}}>not connected</Text>
+                )
+                
             }
         </View>
     )
