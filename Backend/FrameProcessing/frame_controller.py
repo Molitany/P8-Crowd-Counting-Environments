bfc0e2c9894bc37edace0714076fd3acac7ef620
Buenno <krijoh.b@gmail.com>
2023-04-11 13:54:55 +0200

    mostly done i guess

--- Backend/FrameProcessing/frame_controller.py
+++ Backend/FrameProcessing/frame_controller.py
@@ -17,18 +17,18 @@ class MagicFrameProcessor:
     def process(self, frame:np.ndarray) -> Tuple(int, np.ndarray):
         """
         if not calibrated -> calibrate
-         :store magic_func :return none
+         :store magic_func :return annotated image
         else -> p2p -> heatmap
-         :store sampling :return heatmap
+         :store sampling :return count, heatmap
         """
         if not self.__magic: # change todo re-calibration
             return self.__calibrate(frame=frame)
         
         count, head_coords = self.__find_heads(frame=frame)
-        heatmap = self.__create_heatmap(head_coords)
+        heatmap = self.__create_heatmap(frame, head_coords)
         return count, heatmap
 
-    def __calibrate(self, frame, sample_points=6):
+    def __calibrate(self, frame:np.ndarray, sample_points=6):
         self.is_calibrating = True
         if not self.__calibration:
             args = dict({'average_height':173})
@@ -45,12 +45,45 @@ class MagicFrameProcessor:
             self.mode = self.__calibration.mode
             self.__calibration = None
             # do the magic func merge dance here probably..
-        return annotated_frame
+        return -1, annotated_frame
     
-    def __find_heads(self, frame):
+    def __find_heads(self, frame:np.ndarray):
         if not self.__p2p:
             self.__p2p = PersistentP2P()
         return self.__p2p.process(frame=frame)
 
-    def __create_heatmap(self, points:List[List[float,float]]):
-        pass
+    def __create_heatmap(self, frame:np.ndarray, points:List[List[float,float]], overlay:bool=False) -> np.ndarray:
+        # draw the predictions
+        size = 10
+        img_to_draw = np.zeros(frame.shape, np.uint8)
+        for p in points:
+            scaled = self.__magic(p[1]) * size
+            img_to_draw = cv2.circle(img_to_draw, (int(p[0]), int(p[1])), scaled, (255, 255, 255), -1)
+        
+        blur = cv2.GaussianBlur(img_to_draw, (13,13),11)
+        heatmap = cv2.applyColorMap(blur, cv2.COLORMAP_JET)
+
+        if overlay:
+            return cv2.addWeighted(heatmap, 0.5, frame, 0.5, 0)
+        
+        return heatmap
+
+
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture('Crosswalk.mp4')
+    magic = MagicFrameProcessor()
+    while True:
+        success, frame = cap.read()
+        if success:
+            count, img = magic.process(frame=frame)
+            
+            cv2.imshow("YOLOv8 Inference", img)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            # Break the loop if the end of the video is reached
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    
